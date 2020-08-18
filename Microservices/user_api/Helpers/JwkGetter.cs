42302af339737ef91e1ec418717df2ffc7b41838
Martin Petrov <mlpetrov@ucdavis.edu>
2020-08-18 20:58:03 +0300

    got user_api to wait for bouncer to be up as well

--- Microservices/user_api/Helpers/JwkGetter.cs
+++ Microservices/user_api/Helpers/JwkGetter.cs
@@ -25,49 +25,24 @@ namespace dotnetapi.Helpers
 		*/
 		public async Task<JsonWebKey> getJwK()
 		{
-			using var response = await Client.GetAsync(AuthServerUrl + "/auth/jwk", HttpCompletionOption.ResponseHeadersRead);
-
-			response.EnsureSuccessStatusCode();
-			try {
-				
-				var jsonString =  await response.Content.ReadAsStringAsync();
-				return JsonConvert.DeserializeObject<JsonWebKey>(jsonString);
-
-			}
-			catch {
-				System.Console.WriteLine("Http Response could not be deserialized");
-				return new JsonWebKey();
-			}
+			
+			bool authServerUp = false;
+			HttpResponseMessage response;
+			while (!authServerUp) {
+                try {
+                    // migrate any database changes on startup
+					response = await Client.GetAsync(AuthServerUrl + "/auth/jwk", HttpCompletionOption.ResponseHeadersRead);
+                    response.EnsureSuccessStatusCode();
+                    authServerUp = true;
+					var jsonString =  await response.Content.ReadAsStringAsync();
+					return JsonConvert.DeserializeObject<JsonWebKey>(jsonString);
+                } catch {
+                    Console.WriteLine("Auth Server inaccessible... pausing for 5 sec...");
+                    System.Threading.Thread.Sleep(5000);
+                }
+            }
+			return new JsonWebKey();
 		}
-
-		/*
-			This function converts the PEM key we get from secret_bouncer 
-			into a der key.
-		*/
-	/* 	public byte[] GenerateDer() 
-		{
-			string pemContents = getPublicKeyString().Result;
-			const string RsaSpkiHeader = "-----BEGIN PUBLIC KEY-----";
-			const string RsaSpkiFooter = "-----END PUBLIC KEY-----";
-
-			/* Remove the Header and Footers*/
-		/* 	if (pemContents.StartsWith(RsaSpkiHeader)) {
-				int endIdx = pemContents.IndexOf(
-					RsaSpkiFooter,
-					RsaSpkiHeader.Length,
-					StringComparison.Ordinal
-				);
-
-				string base64 = pemContents.Substring(
-					RsaSpkiHeader.Length,
-					endIdx - RsaSpkiHeader.Length
-				);
-				Console.WriteLine(base64);
-				return Convert.FromBase64String(base64);
-			}
-			throw new InvalidOperationException();
-		}
-		 */
 	}
 
 }
