3ba8aab49f34ef0fb76f5aec56d6b053188cc287
Martin Petrov <mlpetrov@ucdavis.edu>
2020-08-23 18:16:44 +0300

    added basics for socket.io

--- Microservices/swapman/server.js
+++ Microservices/swapman/server.js
@@ -3,13 +3,45 @@
 //////////////////////////////////////////////
 require('dotenv').config({path: '.env'});
 
-const App = require('express')();
+const app =  require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
+/* Socket.io Namespaces */
+const mitm = io.of('/mitm');
+const untrusted = io.of('/untrusted');
+const trusted = io.of('/trusted');
 //////////////////////////////////////////////
 /////////////////// Config ///////////////////
 //////////////////////////////////////////////
+http.listen(process.env.SERVER_PORT, () => {
+    console.log('listening on port ' + process.env.SERVER_PORT);
+});
 
-App.listen(process.env.BOUNCER_PORT, ()=> {
-    console.log('Server listening on port ' 
-        + process.env.USMAN_PORT);
+//////////////////////////////////////////////
+/////////////////// Stubs ////////////////////
+//////////////////////////////////////////////
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/stubs/index.html');
+});
+app.get('/mitm', (req, res) => {
+    res.sendFile(__dirname + '/stubs/mitm.html');
+});
+app.get('/trusted', (req, res) => {
+    res.sendFile(__dirname + '/stubs/untrusted.html');
+});
+app.get('/untrusted', (req, res) => {
+    res.sendFile(__dirname + '/stubs/trusted.html');
+});
+
+//////////////////////////////////////////////
+///////////////// Socket.io //////////////////
+//////////////////////////////////////////////
+
+
+mitm.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
 });
