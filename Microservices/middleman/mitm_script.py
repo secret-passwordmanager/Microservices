3552ad3ebb5ee0e9836dca9cfd726506e630e984
Martin <mlpetrov@ucdavis.edu>
2020-09-28 19:17:34 -0700

    got middleman service roughly working

--- Microservices/middleman/mitm_script.py
+++ Microservices/middleman/mitm_script.py
@@ -36,23 +36,24 @@ def swapApproved(swap):
         userSwaps[swap['ip']] = []
     # Push swap into userSwaps
     userSwaps[swap['ip']].append(swap)
-
+    
     # Let swapman know you received the swap
     sio.emit('swapReceived', swap, namespace='/Mitm')
-    
-    print(userSwaps[swap['ip']])
+    print(swap)
+   # print(swap['ip'])
 
 
 def request(flow: http.HTTPFlow) -> None:
-    if flow.request.method == "POST":
-        address = flow.client_conn.address[0]
-        ip = regex.sub(r'^.*:', '', address)
-        host = flow.request.pretty_host
-        ext = tldextract.extract(host)
-        domain = ext.domain + "." + ext.suffix
+  #  if flow.request.method == "POST":
+    address = flow.client_conn.address[0]
+    ip = regex.sub(r'^.*:', '', address)
 
+    ext = tldextract.extract(flow.request.pretty_host)
+    domain = ext.domain + "." + ext.suffix
+
+    if ip in userSwaps:
         swaps = userSwaps[ip]
-
+       # print(domain)
         form = flow.request.urlencoded_form
         
         # Check if token exists in form if it exists
@@ -62,12 +63,18 @@ def request(flow: http.HTTPFlow) -> None:
                 values = form.get_all(key)
 
                 for swap in swaps:
-                    if swap['token'] in values:
+                    if swap['token'] in values and swap['domain'] == domain:
                         form.set_all(key, [swap['credVal']])
                         swap.remove(swap)
                         print('In form swap')
         
 
         for swap in swaps:
-            flow.request.content = flow.request.content.replace(bytes(swap['token'], encoding='utf-8'), bytes(swap['credVal'], encoding='utf-8'))
-            print('in normal swap')
+            # Check if domain is correct
+            if swap['domain'] == domain:
+                flow.request.content = flow.request.content.replace(bytes(swap['token'], encoding='utf-8'), bytes(swap['credVal'], encoding='utf-8'))
+                print(swap['token'])
+    
+    else:
+       # print(ip)
+        print('Ip not found')
